287a299fe42778fc152447500c346c1d1cbebbca
Francesc Elies Henar <felies@visualfabriq.com>
2015-01-06 15:10:27 +0100

    test_groupby_04

--- bquery/tests/test_ctable.py
+++ bquery/tests/test_ctable.py
@@ -176,7 +176,53 @@ class TestCtable():
         assert_list_equal(
             [list(x) for x in result_bcolz], ref)
 
-    # TODO
-    # test_groupby_04: Test groupby's aggregation
-    #                      (groupby over multiple rows results
-    #                      into multiple groups)
+    def test_groupby_04(self):
+        """
+        test_groupby_04: Test groupby's aggregation
+                             (groupby over multiple rows results
+                             into multiple groups)
+        """
+        random.seed(1)
+
+        groupby_cols = ['f0', 'f1', 'f2']
+        groupby_lambda = lambda x: [x[0], x[1], x[2]]
+        agg_list = ['f4', 'f5', 'f6']
+        agg_lambda = lambda x: [x[4], x[5], x[6]]
+        num_rows = 2000
+
+        # -- Data --
+        g = self.gen_almost_unique_row(num_rows)
+        data = np.fromiter(g, dtype='S1,f8,i8,i4,f8,i8,i4')
+
+        # -- Bcolz --
+        print('--> Bcolz')
+        self.rootdir = tempfile.mkdtemp(prefix='bcolz-')
+        os.rmdir(self.rootdir)  # folder should be emtpy
+        fact_bcolz = bquery.ctable(data, rootdir=self.rootdir)
+        fact_bcolz.flush()
+
+        fact_bcolz.cache_factor(groupby_cols, refresh=True)
+        result_bcolz = fact_bcolz.groupby(groupby_cols, agg_list)
+        print result_bcolz
+
+        # Itertools result
+        print('--> Itertools')
+        result_itt = self.helper_itt_groupby(data, groupby_lambda)
+        uniquekeys = result_itt['uniquekeys']
+        print uniquekeys
+
+        ref = []
+        for item in result_itt['groups']:
+            f4 = 0
+            f5 = 0
+            f6 = 0
+            for row in item:
+                f0 = groupby_lambda(row)
+                f4 += row[4]
+                f5 += row[5]
+                f6 += row[6]
+            ref.append(f0 + [f4, f5, f6])
+
+        assert_list_equal(
+            sorted([list(x) for x in result_bcolz]),
+            sorted(ref))
