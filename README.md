87f7e6a5b81b49f11d80d32e6d27a8b989d8b91b
Francesc Elies Henar <felies@visualfabriq.com>
2015-01-10 19:25:18 +0100

    update benchmark + results to readme

--- README.md
+++ README.md
@@ -31,10 +31,43 @@ Testing
 
 Benchmarks
 ----------
-Short benchmark to compare bquery, cytoolz & pandas
+Short benchmark to compare bquery, cytoolz & pandas  
 ```python bquery/benchmarks/bench_groupby.py```
+
+Results might vary depending on where testing is performed  
+
+Note: ctable is in this case on-disk storage vs pandas in-memory  
+
+```
+Groupby on column 'f0'
+Aggregation results on column 'f2'
+Rows:  1000000
+
+ctable((1000000,), [('f0', 'S2'), ('f1', 'S2'), ('f2', '<i8'), ('f3', '<i8')])
+  nbytes: 19.07 MB; cbytes: 1.14 MB; ratio: 16.70
+  cparams := cparams(clevel=5, shuffle=True, cname='blosclz')
+  rootdir := '/var/folders/_y/zgh0g75d13d65nd9_d7x8llr0000gn/T/bcolz-LaL2Hn'
+[('ES', 'b1', 1, -1) ('NL', 'b2', 2, -2) ('ES', 'b3', 1, -1) ...,
+ ('NL', 'b3', 2, -2) ('ES', 'b4', 1, -1) ('NL', 'b5', 2, -2)]
+
+
+cytoolz over bcolz:  1.8483 sec
+x28.6969 slower than pandas
+{'NL': 1000000, 'ES': 500000}
+
+
+bquery over bcolz:  0.1969 sec
+x3.0573 slower than pandas
+[('ES', 500000) ('NL', 1000000)]
+
+
+bquery over bcolz (factorization cached):  0.1283 sec
+x1.992 slower than pandas
+[('ES', 500000) ('NL', 1000000)]
+```
+For details about these results see please the python script
 
 Performance (vbench)
 --------------------
-Run vbench suite
+Run vbench suite  
 ```python bquery/benchmarks/vb_suite/run_suite.py```
--- bquery/benchmarks/bench_groupby.py
+++ bquery/benchmarks/bench_groupby.py
@@ -14,16 +14,19 @@ import tempfile
 import os
 import time
 
-
+t_elapsed = 0.0
 @contextlib.contextmanager
 def ctime(message=None):
     "Counts the time spent in some context"
+    global t_elapsed
+    t_elapsed = 0.0
     print('\n')
     t = time.time()
     yield
     if message:
         print(message + ":  ", end='')
-    print(round(time.time() - t, 4), "sec")
+    t_elapsed = time.time() - t
+    print(round(t_elapsed, 4), "sec")
 
 
 ga = itt.cycle(['ES', 'NL'])
@@ -35,27 +38,38 @@ os.rmdir(rootdir)  # tests needs this cleared
 
 n_rows = 1000000
 print('Rows: ', n_rows)
+
+# -- data
 z = np.fromiter(((a, b, x, y) for a, b, x, y in itt.izip(ga, gb, gx, gy)),
                 dtype='S2,S2,i8,i8', count=n_rows)
 
 ct = bquery.ctable(z, rootdir=rootdir, )
+print(ct)
+
+# -- pandas --
+df = pd.DataFrame(z)
+with ctime(message='pandas'):
+    result = df.groupby(['f0'])['f2'].sum()
+print(result)
+t_pandas = t_elapsed
 
 # -- cytoolz --
-with ctime(message='cytoolz'):
+with ctime(message='cytoolz over bcolz'):
     # In Memory Split-Apply-Combine
     # http://toolz.readthedocs.org/en/latest/streaming-analytics.html?highlight=reduce#split-apply-combine-with-groupby-and-reduceby
     r = cytoolz.groupby(lambda row: row.f0, ct)
     result = valmap(compose(sum, pluck(2)), r)
+print('x{0} slower than pandas'.format(round(t_elapsed/t_pandas, 4)))
 print(result)
 
 # -- bcolz --
-with ctime(message='bcolz'):
-    ct.cache_factor(['f0'], refresh=True)
+with ctime(message='bquery over bcolz'):
     result = ct.groupby(['f0'], ['f2'])
+print('x{0} slower than pandas'.format(round(t_elapsed/t_pandas, 4)))
 print(result)
 
-# -- pandas --
-with ctime(message='pandas'):
-    df = pd.DataFrame(z)
-    result = df.groupby(['f0'])['f2'].sum()
+ct.cache_factor(['f0'], refresh=True)
+with ctime(message='bquery over bcolz (factorization cached)'):
+    result = ct.groupby(['f0'], ['f2'])
+print('x{0} slower than pandas'.format(round(t_elapsed/t_pandas, 4)))
 print(result)
