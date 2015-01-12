08e177da67a99bc238e9091ccb8df7b851d69a7f
Francesc Elies Henar <felies@visualfabriq.com>
2015-01-12 17:47:25 +0100

    added benchmark results blaze + bcolz

--- README.md
+++ README.md
@@ -104,25 +104,32 @@ ctable((1000000,), [('f0', 'S2'), ('f1', 'S2'), ('f2', '<i8'), ('f3', '<i8')])
  ('NL', 'b3', 2, -2) ('ES', 'b4', 1, -1) ('NL', 'b5', 2, -2)]
 
 
-pandas:  0.0703 sec
+pandas:  0.0827 sec
 f0
 ES     500000
 NL    1000000
 Name: f2, dtype: int64
 
 
-cytoolz over bcolz:  1.7976 sec
-x25.5802 slower than pandas
+cytoolz over bcolz:  1.8612 sec
+x22.5 slower than pandas
 {'NL': 1000000, 'ES': 500000}
 
 
-bquery over bcolz:  0.1876 sec
-x2.6697 slower than pandas
+blaze over bcolz:  0.2983 sec
+x3.61 slower than pandas
+   f0   sum_f2
+0  ES   500000
+1  NL  1000000
+
+
+bquery over bcolz:  0.1833 sec
+x2.22 slower than pandas
 [('ES', 500000) ('NL', 1000000)]
 
 
-bquery over bcolz (factorization cached):  0.1426 sec
-x2.0292 slower than pandas
+bquery over bcolz (factorization cached):  0.1302 sec
+x1.57 slower than pandas
 [('ES', 500000) ('NL', 1000000)]
 ```
 For details about these results see please the python script
--- bquery/benchmarks/bench_groupby.py
+++ bquery/benchmarks/bench_groupby.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 # bench related imports
 import numpy as np
+import shutil
 import bquery
 import pandas as pd
 import itertools as itt
@@ -8,6 +9,7 @@ import cytoolz
 import cytoolz.dicttoolz
 from toolz import valmap, compose
 from cytoolz.curried import pluck
+import blaze as blz
 # other imports
 import contextlib
 import tempfile
@@ -33,8 +35,9 @@ ga = itt.cycle(['ES', 'NL'])
 gb = itt.cycle(['b1', 'b2', 'b3', 'b4', 'b5'])
 gx = itt.cycle([1, 2])
 gy = itt.cycle([-1, -2])
-rootdir = tempfile.mkdtemp(prefix='bcolz-')
-os.rmdir(rootdir)  # tests needs this cleared
+rootdir = 'bench-data.bcolz'
+if os.path.exists(rootdir):
+    shutil.rmtree(rootdir)
 
 n_rows = 1000000
 print('Rows: ', n_rows)
@@ -59,17 +62,28 @@ with ctime(message='cytoolz over bcolz'):
     # http://toolz.readthedocs.org/en/latest/streaming-analytics.html?highlight=reduce#split-apply-combine-with-groupby-and-reduceby
     r = cytoolz.groupby(lambda row: row.f0, ct)
     result = valmap(compose(sum, pluck(2)), r)
-print('x{0} slower than pandas'.format(round(t_elapsed/t_pandas, 4)))
+print('x{0} slower than pandas'.format(round(t_elapsed/t_pandas, 2)))
 print(result)
 
-# -- bcolz --
+# -- blaze + bcolz --
+# from IPython import embed; embed(); exit()
+blaze_data = blz.Data(ct.rootdir)
+expr = blz.by(blaze_data.f0, sum_f2=blaze_data.f2.sum())
+with ctime(message='blaze over bcolz'):
+    result = blz.compute(expr)
+print('x{0} slower than pandas'.format(round(t_elapsed/t_pandas, 2)))
+print(result)
+
+# -- bquery --
 with ctime(message='bquery over bcolz'):
     result = ct.groupby(['f0'], ['f2'])
-print('x{0} slower than pandas'.format(round(t_elapsed/t_pandas, 4)))
+print('x{0} slower than pandas'.format(round(t_elapsed/t_pandas, 2)))
 print(result)
 
 ct.cache_factor(['f0'], refresh=True)
 with ctime(message='bquery over bcolz (factorization cached)'):
     result = ct.groupby(['f0'], ['f2'])
-print('x{0} slower than pandas'.format(round(t_elapsed/t_pandas, 4)))
+print('x{0} slower than pandas'.format(round(t_elapsed/t_pandas, 2)))
 print(result)
+
+shutil.rmtree(rootdir)
