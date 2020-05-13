fc050bb844a13f33808d482d46757e4ceca4548e
Yasser Lebon <ylebon@visualfabriq.com>
2020-05-13 17:33:46 +0200

    Refactor bcolz files creation

--- bquery/benchmarks/taxi/load.py
+++ bquery/benchmarks/taxi/load.py
@@ -2,64 +2,81 @@ import urllib
 import glob
 import pandas as pd
 from bquery import ctable
+import os
 
-
-def download_data(workdir):
+def download_data(workdir, year_list=None, month_list=None, force=False):
     # wget https://storage.googleapis.com/tlc-trip-data/2015/yellow_tripdata_2015-01.csv
-    for year in [2015]:
-        for month in range(1, 13):
+    year_list = year_list or [2015]
+    month_list = month_list or range(1, 13)
+    result = list()
+    for year in year_list:
+        for month in month_list:
             filename  = 'yellow_tripdata_{0}-{1:02d}.csv'.format(year, month)
             url = f'https://s3.amazonaws.com/nyc-tlc/trip+data/{filename}'
-            print(url)
-            urllib.request.urlretrieve(url, workdir + '/' + filename)
+            output_file = os.path.join(workdir, filename)
+            print("url: {0}".format(url))
+            print("output file: {0}".format(output_file))
+            if not os.path.exists(output_file) or force:
+                urllib.request.urlretrieve(url, output_file)
+            result.append(output_file)
+    return result
 
 
+def load_file(filename):
+    print("loading: {0}".format(filename))
+    
+    import_df = pd.read_csv(filename)
+
+    # lower columns because of input inconsistencies
+    import_df.columns = [x.lower() for x in import_df.columns]
+    import_df.columns = [x.strip() for x in import_df.columns]
+    import_df.columns = [x.replace('tpep_', '') for x in import_df.columns]
+
+    import_df['nr_rides'] = 1
+
+    import_df['pickup_date'] = import_df['pickup_datetime'].str[0:10]
+    import_df['pickup_date'] = import_df['pickup_date'].str.replace('-', '')
+    import_df['pickup_year'] = import_df['pickup_date'].str[0:4].astype(int)
+    import_df['pickup_yearmonth'] = import_df['pickup_date'].str[0:6].astype(int)
+    import_df['pickup_month'] = import_df['pickup_date'].str[4:6].astype(int)
+    import_df['pickup_date'] = import_df['pickup_date'].astype(int)
+    import_df['pickup_time'] = import_df['pickup_datetime'].str[11:]
+    import_df['pickup_time'] = import_df['pickup_time'].str.replace(':', '')
+    import_df['pickup_hour'] = import_df['pickup_time'].str[0:2].astype(int)
+    import_df['pickup_time'] = import_df['pickup_time'].astype(int)
+    del import_df['pickup_datetime']
+
+    import_df['dropoff_date'] = import_df['dropoff_datetime'].str[0:10]
+    import_df['dropoff_date'] = import_df['dropoff_date'].str.replace('-', '')
+    import_df['dropoff_year'] = import_df['dropoff_date'].str[0:4].astype(int)
+    import_df['dropoff_yearmonth'] = import_df['dropoff_date'].str[0:6].astype(int)
+    import_df['dropoff_month'] = import_df['dropoff_date'].str[4:6].astype(int)
+    import_df['dropoff_date'] = import_df['dropoff_date'].astype(int)
+    import_df['dropoff_time'] = import_df['dropoff_datetime'].str[11:]
+    import_df['dropoff_time'] = import_df['dropoff_time'].str.replace(':', '')
+    import_df['dropoff_hour'] = import_df['dropoff_time'].str[0:2].astype(int)
+    import_df['dropoff_time'] = import_df['dropoff_time'].astype(int)
+    del import_df['dropoff_datetime']
+    
+    return import_df
+    
 def create_bcolz(workdir):
-    file_list = sorted(glob.glob(workdir + 'yellow_tripdata_*.csv'))
+    csv_search_pattern = os.path.join(workdir, "yellow_tripdata_*.csv")
+    print("csv search pattern: {0}".format(csv_search_pattern))
+    file_list = sorted(glob.glob(csv_search_pattern))
     if not file_list:
         raise ValueError('No Files Found')
 
     expected_len = len(file_list) * 50000000
 
+    rootdir=os.path.join(workdir, 'bcolz')
+
     for i, filename in enumerate(file_list):
 
-        print(filename)
-
-        import_df = pd.read_csv(filename)
-
-        # lower columns because of input inconsistencies
-        import_df.columns = [x.lower() for x in import_df.columns]
-        import_df.columns = [x.strip() for x in import_df.columns]
-        import_df.columns = [x.replace('tpep_', '') for x in import_df.columns]
-
-        import_df['nr_rides'] = 1
-
-        import_df['pickup_date'] = import_df['pickup_datetime'].str[0:10]
-        import_df['pickup_date'] = import_df['pickup_date'].str.replace('-', '')
-        import_df['pickup_year'] = import_df['pickup_date'].str[0:4].astype(int)
-        import_df['pickup_yearmonth'] = import_df['pickup_date'].str[0:6].astype(int)
-        import_df['pickup_month'] = import_df['pickup_date'].str[4:6].astype(int)
-        import_df['pickup_date'] = import_df['pickup_date'].astype(int)
-        import_df['pickup_time'] = import_df['pickup_datetime'].str[11:]
-        import_df['pickup_time'] = import_df['pickup_time'].str.replace(':', '')
-        import_df['pickup_hour'] = import_df['pickup_time'].str[0:2].astype(int)
-        import_df['pickup_time'] = import_df['pickup_time'].astype(int)
-        del import_df['pickup_datetime']
-
-        import_df['dropoff_date'] = import_df['dropoff_datetime'].str[0:10]
-        import_df['dropoff_date'] = import_df['dropoff_date'].str.replace('-', '')
-        import_df['dropoff_year'] = import_df['dropoff_date'].str[0:4].astype(int)
-        import_df['dropoff_yearmonth'] = import_df['dropoff_date'].str[0:6].astype(int)
-        import_df['dropoff_month'] = import_df['dropoff_date'].str[4:6].astype(int)
-        import_df['dropoff_date'] = import_df['dropoff_date'].astype(int)
-        import_df['dropoff_time'] = import_df['dropoff_datetime'].str[11:]
-        import_df['dropoff_time'] = import_df['dropoff_time'].str.replace(':', '')
-        import_df['dropoff_hour'] = import_df['dropoff_time'].str[0:2].astype(int)
-        import_df['dropoff_time'] = import_df['dropoff_time'].astype(int)
-        del import_df['dropoff_datetime']
-
+        import_df = load_file(filename)
+        
         if i == 0:
-            import_ct = ctable.fromdataframe(import_df, rootdir=workdir + 'taxi', expectedlen=expected_len, mode='w')
+            import_ct = ctable.fromdataframe(import_df, rootdir=rootdir, expectedlen=expected_len, mode='w')
             del import_df
         else:
             temp_ct = ctable.fromdataframe(import_df)
@@ -69,7 +86,7 @@ def create_bcolz(workdir):
 
     import_ct.flush()
 
-    import_ct = ctable(rootdir=workdir + 'taxi', mode='a')
+    import_ct = ctable(rootdir=rootdir, mode='a')
 
     import_ct.cache_factor([
         'dropoff_date',
@@ -100,47 +117,9 @@ def create_bcolz_chunks(workdir):
         raise ValueError('No Files Found')
 
     for i, filename in enumerate(file_list):
-        print(filename)
-
-        rootdir = workdir + 'taxi_' + str(i)
-
-        import_df = pd.read_csv(filename)
-
-        # lower columns because of input inconsistencies
-        import_df.columns = [x.lower() for x in import_df.columns]
-        import_df.columns = [x.strip() for x in import_df.columns]
-        import_df.columns = [x.replace('tpep_', '') for x in import_df.columns]
-
-        import_df['nr_rides'] = 1
-
-        import_df['pickup_date'] = import_df['pickup_datetime'].str[0:10]
-        import_df['pickup_date'] = import_df['pickup_date'].str.replace('-', '')
-        import_df['pickup_year'] = import_df['pickup_date'].str[0:4].astype(int)
-        import_df['pickup_yearmonth'] = import_df['pickup_date'].str[0:6].astype(int)
-        import_df['pickup_month'] = import_df['pickup_date'].str[4:6].astype(int)
-        import_df['pickup_date'] = import_df['pickup_date'].astype(int)
-        import_df['pickup_time'] = import_df['pickup_datetime'].str[11:]
-        import_df['pickup_time'] = import_df['pickup_time'].str.replace(':', '')
-        import_df['pickup_hour'] = import_df['pickup_time'].str[0:2].astype(int)
-        import_df['pickup_time'] = import_df['pickup_time'].astype(int)
-        del import_df['pickup_datetime']
-
-        import_df['dropoff_date'] = import_df['dropoff_datetime'].str[0:10]
-        import_df['dropoff_date'] = import_df['dropoff_date'].str.replace('-', '')
-        import_df['dropoff_year'] = import_df['dropoff_date'].str[0:4].astype(int)
-        import_df['dropoff_yearmonth'] = import_df['dropoff_date'].str[0:6].astype(int)
-        import_df['dropoff_month'] = import_df['dropoff_date'].str[4:6].astype(int)
-        import_df['dropoff_date'] = import_df['dropoff_date'].astype(int)
-        import_df['dropoff_time'] = import_df['dropoff_datetime'].str[11:]
-        import_df['dropoff_time'] = import_df['dropoff_time'].str.replace(':', '')
-        import_df['dropoff_hour'] = import_df['dropoff_time'].str[0:2].astype(int)
-        import_df['dropoff_time'] = import_df['dropoff_time'].astype(int)
-        del import_df['dropoff_datetime']
-
-        import_ct = ctable.fromdataframe(import_df,
-                                         rootdir=rootdir,
-                                         expectedlen=len(import_df),
-                                         mode='w')
+        rootdir = os.path.join(workdir, 'bcolz_chunk', 'taxi_{0}'.format(i))
+        import_df = load_file(filename)
+        import_ct = ctable.fromdataframe(import_df, rootdir=rootdir, expectedlen=len(import_df), mode='w')
         del import_df
 
         import_ct.flush()
